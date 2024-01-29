28b4674e2f5527581c7296bdd04ddc310a274f30
mockingjay <8400684@qq.com>
2024-01-30 00:30:10 +0800

    ini

---
+++ SocketServer/Program.cs
@@ -0,0 +1,24 @@
+﻿using Kestrel.Core;
+using Kestrel.Core.Messages;
+using KestrelCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SocketServer;
+using SuperSocket;
+using SuperSocket.Command;
+using SuperSocket.IOCPTcpChannelCreatorFactory;
+
+var host = SuperSocketHostBuilder.Create<CommandMessage, CommandFilterPipeLine>()
+    .UsePackageEncoder<CommandEncoder>()
+    .UsePackageDecoder<CommandDecoder>()
+    .UseCommand(options => options.AddCommand<LoginCommand>())
+    .UseSessionFactory<TestSessionFactory>()
+    .UseIOCPTcpChannelCreatorFactory()
+    .UseInProcSessionContainer()
+    .ConfigureServices((_, service) =>
+    {
+        service.AddSingleton<IMessageFactoryPool, CommandMessageFactoryPool>();
+    })
+    .Build();
+
+await host.RunAsync();
