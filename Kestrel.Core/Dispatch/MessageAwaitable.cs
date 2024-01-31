4daa867c38a8690b62e1406f3316572f27636ebb
mockingjay <8400684@qq.com>
2024-01-31 18:06:23 +0800

    ini

--- Kestrel.Core/Dispatch/MessageAwaitable.cs
+++ Kestrel.Core/Dispatch/MessageAwaitable.cs
@@ -5,97 +5,69 @@ using KestrelCore;
 namespace Kestrel.Core;
 
 public sealed class MessageAwaitable<TMessage>(uint packetIdentifier, MessageDispatcher owningMessageDispatcher)
-    : IMessageAwaitable
+    : IMessageAwaitable, IValueTaskSource<CommandMessage>
     where TMessage : IMessage<TMessage>
 {
-    //private ManualResetValueTaskSourceCore<CommandMessage> _taskSourceCore = new();
-
-    private readonly TaskCompletionSource<CommandMessage> _promise =
-        new(TaskCreationOptions.RunContinuationsAsynchronously);
+    private ManualResetValueTaskSourceCore<CommandMessage> _taskSourceCore = new()
+    {
+        RunContinuationsAsynchronously = true,
+    };
 
     public async Task<ValueCommandResponse<TMessage>> WaitAsync(CancellationToken cancellationToken)
     {
         cancellationToken.ThrowIfCancellationRequested();
 
         //await using var register = cancellationToken.Register(() => Fail(new TimeoutException()));
-        var message = await _promise.Task.ConfigureAwait(false);
+
+        var message = await new ValueTask<CommandMessage>(this, _taskSourceCore.Version);
 
         return new ValueCommandResponse<TMessage>(message);
     }
 
-    // #region ValueTask
-    //
-    // public async ValueTask<ValueCommandResponse<TMessage>> WaitForValueTaskAsync(CancellationToken cancellationToken)
-    // {
-    //     cancellationToken.ThrowIfCancellationRequested();
-    //
-    //     _taskSourceCore.Reset();
-    //     
-    //     await using var register = cancellationToken.Register(() => Fail(new TimeoutException()));
-    //
-    //     var message = await new ValueTask<CommandMessage>(this, _taskSourceCore.Version);
-    //
-    //     return new ValueCommandResponse<TMessage>(message);
-    // }
-    //
-    // public void CompleteForValueTask(CommandMessage message)
-    // {
-    //     ArgumentNullException.ThrowIfNull(message);
-    //
-    //     _taskSourceCore.SetResult(message);
-    // }
-    //
-    // public void FailForValueTask(Exception exception)
-    // {
-    //     ArgumentNullException.ThrowIfNull(exception);
-    //
-    //     _taskSourceCore.SetException(exception);
-    // }
-    //
-    // public void CancelForValueTask()
-    // {
-    //     _taskSourceCore.Reset();
-    // }
-    //
-    // #endregion
+    public async ValueTask<ValueCommandResponse<TMessage>> WaitForValueTaskAsync(CancellationToken cancellationToken)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+
+        //await using var register = cancellationToken.Register(() => Fail(new TimeoutException()));
+
+        var message = await new ValueTask<CommandMessage>(this, _taskSourceCore.Version);
+
+        return new ValueCommandResponse<TMessage>(message);
+    }
 
     public void Complete(CommandMessage message)
     {
-        ArgumentNullException.ThrowIfNull(message);
-
-        _promise.TrySetResult(message);
+        _taskSourceCore.SetResult(message);
     }
 
     public void Fail(Exception exception)
     {
-        ArgumentNullException.ThrowIfNull(exception);
-
-        _promise.TrySetException(exception);
+        _taskSourceCore.SetException(exception);
     }
 
     public void Cancel()
     {
-        _promise.TrySetCanceled();
+        _taskSourceCore.SetException(new TaskCanceledException());
     }
 
     public void Dispose()
     {
         owningMessageDispatcher.RemoveAwaitable(packetIdentifier);
     }
-    //
-    // CommandMessage IValueTaskSource<CommandMessage>.GetResult(short token)
-    // {
-    //     return _taskSourceCore.GetResult(token);
-    // }
-    //
-    // ValueTaskSourceStatus IValueTaskSource<CommandMessage>.GetStatus(short token)
-    // {
-    //     return _taskSourceCore.GetStatus(token);
-    // }
-    //
-    // void IValueTaskSource<CommandMessage>.OnCompleted(Action<object?> continuation, object? state, short token,
-    //     ValueTaskSourceOnCompletedFlags flags)
-    // {
-    //     _taskSourceCore.OnCompleted(continuation, state, token, flags);
-    // }
+
+    CommandMessage IValueTaskSource<CommandMessage>.GetResult(short token)
+    {
+        return _taskSourceCore.GetResult(token);
+    }
+
+    ValueTaskSourceStatus IValueTaskSource<CommandMessage>.GetStatus(short token)
+    {
+        return _taskSourceCore.GetStatus(token);
+    }
+
+    void IValueTaskSource<CommandMessage>.OnCompleted(Action<object?> continuation, object? state, short token,
+        ValueTaskSourceOnCompletedFlags flags)
+    {
+        _taskSourceCore.OnCompleted(continuation, state, token, flags);
+    }
 }
